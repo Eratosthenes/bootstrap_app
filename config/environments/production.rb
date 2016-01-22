15c5a6678775a4fbf40a71144537a3b3bb6a2ac8
Nathan Rapport <n.rapport@gmail.com>
2016-01-22 05:36:35 -0800

    changed production to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'wildplantid.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
     :address => "smtp.sendgrid.net",
     :port => 587,
     :domain => "heroku.com",
