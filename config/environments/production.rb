8f3b59a3cee59f69122ce9f9f9a0faaf59475923
Nathan Rapport <n.rapport@gmail.com>
2016-01-21 20:49:54 -0800

    production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,14 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'wildplantid.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = { :address => "smtp.gmail.com",
-     :port => "587", :domain => "gmail.com", :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"], :authentication => "plain", :enable_starttls_auto => true}
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
+    :authentication => "plain",
+    :enable_starttls_auto => true }
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
